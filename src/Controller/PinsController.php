d5deed32edfdde7585e90a55b846906cd9c0c836
RiadhTaleb <talebriadhdz@gmail.com>
2023-01-30 20:25:54 +0100

    adding registration concept

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -4,14 +4,15 @@ namespace App\Controller;
 
 use App\Entity\Pin;
 use App\Form\PinType;
-
 use App\Repository\PinRepository;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PinsController extends AbstractController
 {
@@ -50,5 +51,55 @@ class PinsController extends AbstractController
         return $this->render('pins/create.html.twig', [
             'form' => $form->createView()
         ]);
+    }
+
+
+    #[Route("/pins/{id<[0-9]+>}", name: "app_pins_show", methods: ["GET"])]
+    public function show(Pin $pin): Response
+    {
+        return $this->render('pins/show.html.twig', compact('pin'));
+    }
+
+
+    /**
+     * @Route("/pins/{id<[0-9]+>}/edit", name="app_pins_edit", methods={"GET","PUT"})
+     * @IsGranted("PIN_MANAGE", subject="pin")
+     */
+    public function edit(Request $request, Pin $pin, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(PinType::class, $pin, [
+            'method' => 'PUT'
+        ]);
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $em->flush();
+
+            $this->addFlash('success', 'Pin successfully updated!');
+
+            return $this->redirectToRoute('app_home');
+        }
+        return $this->render('pins/edit.html.twig', [
+            'pin' => $pin,
+            'form' => $form->createView()
+        ]);
+    }
+
+
+
+    #[Route("/pins/{id<[0-9]+>}", name: "app_pins_delete", methods: ["DELETE"])]
+    public function delete(Request $request, Pin $pin, EntityManagerInterface $em)
+    {
+        $this->denyAccessUnlessGranted('PIN_MANAGE', $pin);
+        if($this->isCsrfTokenValid('pin_deletion_' . $pin->getId(), $request->request->get('csrf_token')))
+        {
+            $em->remove($pin);
+            $em->flush();
+
+            $this->addFlash('info', 'Pin successfully deleted!');
+        }
+
+        return $this->redirectToRoute('app_home');
     }
 }
