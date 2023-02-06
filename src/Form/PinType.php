f4d12c59e7dd73530eec1f5814a97ba4083c6b36
RiadhTaleb <talebriadhdz@gmail.com>
2023-02-06 18:36:36 +0100

    adding reset password

--- src/Form/PinType.php
+++ src/Form/PinType.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace App\Form;
 
 use App\Entity\Pin;
@@ -11,21 +10,21 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
-
 class PinType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('imageFile', VichImageType::class, [
-                'label'=> 'Image (JPG or PNG file)',
+                'label' => 'Image (JPG or PNG file)',
                 'required' => false,
                 'allow_delete' => true,
-                'download_url' => false,
+                'download_uri' => false,
                 'imagine_pattern' => 'squared_thumbnail_small'
             ])
             ->add('title', TextType::class)
-            ->add('description', TextareaType::class);
+            ->add('description', TextareaType::class)
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
